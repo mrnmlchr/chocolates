586513ce085c2a14faa1b58a17c7a2a5cf1cffed
Rheamelchor <rheamelchor@gmail.com>
2025-08-24 12:15:19 +0800

    Initial commit

---
+++ chocolate.js
@@ -0,0 +1,94 @@
+const http = require('http');
+const mysql = require('mysql2');
+const url = require('url');
+
+// Create DB connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'chocolates_db'  // changed database name
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Connected to MySQL database');
+});
+
+// Create server
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const query = parsedUrl.query;
+  const method = req.method;
+
+  // Get all chocolates or a single chocolate by id
+  if (path === '/chocolates' && method === 'GET') {
+    if (query.id) {
+      db.query('SELECT * FROM chocolates WHERE id = ?', [query.id], (err, result) => {
+        if (err) throw err;
+        res.end(JSON.stringify(result[0] || {}));
+      });
+    } else {
+      db.query('SELECT * FROM chocolates', (err, result) => {
+        if (err) throw err;
+        res.end(JSON.stringify(result));
+      });
+    }
+  }
+
+  // Insert new chocolate
+  else if (path === '/chocolates' && method === 'POST') {
+    let body = '';
+    req.on('data', chunk => body += chunk);
+    req.on('end', () => {
+      const { chocolate_name, brand, type_of_chocolate, flavor_variant, country_of_origin } = JSON.parse(body);
+      db.query(
+        'INSERT INTO chocolates (chocolate_name, brand, type_of_chocolate, flavor_variant, country_of_origin) VALUES (?, ?, ?, ?, ?)',
+        [chocolate_name, brand, type_of_chocolate, flavor_variant, country_of_origin],
+        (err, result) => {
+          if (err) throw err;
+          res.end(JSON.stringify({ id: result.insertId, chocolate_name, brand, type_of_chocolate, flavor_variant, country_of_origin }));
+        }
+      );
+    });
+  }
+
+  // Update chocolate
+  else if (path === '/chocolates' && method === 'PUT') {
+    let body = '';
+    req.on('data', chunk => body += chunk);
+    req.on('end', () => {
+      const { chocolate_name, brand, type_of_chocolate, flavor_variant, country_of_origin } = JSON.parse(body);
+      db.query(
+        'UPDATE chocolates SET chocolate_name=?, brand=?, type_of_chocolate=?, flavor_variant=?, country_of_origin=? WHERE id=?',
+        [chocolate_name, brand, type_of_chocolate, flavor_variant, country_of_origin, query.id],
+        (err, result) => {
+          if (err) throw err;
+          res.end(JSON.stringify({ id: query.id, chocolate_name, brand, type_of_chocolate, flavor_variant, country_of_origin }));
+        }
+      );
+    });
+  }
+
+  // Delete chocolate
+  else if (path === '/chocolates' && method === 'DELETE') {
+    db.query('DELETE FROM chocolates WHERE id=?', [query.id], (err, result) => {
+      if (err) throw err;
+      res.end(JSON.stringify({ message: 'Chocolate deleted' }));
+    });
+  }
+
+  else {
+    res.statusCode = 404;
+    res.end(JSON.stringify({ error: 'Route not found' }));
+  }
+});
+
+// Start server
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
